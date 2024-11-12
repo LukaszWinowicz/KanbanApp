118dffdf5048ac33088f4a3f6ebfe5ec790b6085
LukaszWinowicz <lwinowicz@gmail.com>
2024-11-12 20:36:02 +0100

    update migration

--- KanbanApp.API/Migrations/20241112193536_ChangeRelationReadingsLocationToReadingsScale.Designer.cs
+++ KanbanApp.API/Migrations/20241112193536_ChangeRelationReadingsLocationToReadingsScale.Designer.cs
@@ -4,6 +4,7 @@ using KanbanApp.API.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace KanbanApp.API.Migrations
 {
     [DbContext(typeof(KanbanDbContext))]
-    partial class KanbanDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241112193536_ChangeRelationReadingsLocationToReadingsScale")]
+    partial class ChangeRelationReadingsLocationToReadingsScale
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ KanbanApp.API/Migrations/20241112193536_ChangeRelationReadingsLocationToReadingsScale.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KanbanApp.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeRelationReadingsLocationToReadingsScale : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Readings_Locations_LocationId",
+                table: "Readings");
+
+            migrationBuilder.RenameColumn(
+                name: "LocationId",
+                table: "Readings",
+                newName: "ScaleId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Readings_LocationId",
+                table: "Readings",
+                newName: "IX_Readings_ScaleId");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Factor10Pcs",
+                table: "Items",
+                type: "decimal(10,2)",
+                precision: 10,
+                scale: 2,
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Readings_Scales_ScaleId",
+                table: "Readings",
+                column: "ScaleId",
+                principalTable: "Scales",
+                principalColumn: "ScaleId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Readings_Scales_ScaleId",
+                table: "Readings");
+
+            migrationBuilder.DropColumn(
+                name: "Factor10Pcs",
+                table: "Items");
+
+            migrationBuilder.RenameColumn(
+                name: "ScaleId",
+                table: "Readings",
+                newName: "LocationId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Readings_ScaleId",
+                table: "Readings",
+                newName: "IX_Readings_LocationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Readings_Locations_LocationId",
+                table: "Readings",
+                column: "LocationId",
+                principalTable: "Locations",
+                principalColumn: "LocationId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
